9494ddbc3106374579f4a10ed4405fe6af820328
FireCoder1092 <smaranpreethesh@gmail.com>
2025-02-15 15:48:24 +1300

    second commit

---
+++ pages/About.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.write('Hello this app is for my mum and dad (for free of course) and so they have been using it and they have been amazed by me (think so) and so thank you visiting the site!')
